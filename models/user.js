fdfc1e1a29008d1f5318da4a4c42149fe263a656
balaji24k <balaji325s@gmail.com>
2023-10-10 22:18:09 +0530

    Task-5

--- models/user.js
+++ models/user.js
@@ -45,6 +45,39 @@ class User {
       )
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => i.productId);
+    return db
+      .collection('products')
+      .find({_id: {$in: productIds}})
+      .toArray()
+      .then((products) => {
+        return products.map(p => {
+          return {
+            ...p, 
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          }
+        })
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(
+      item => item.productId.toString() !== productId.toString()
+    );
+    const db = getDb();
+    return db.collection('users')
+      .updateOne(
+        { _id : new ObjectId(this._id)}, 
+        { $set: {cart : {items: updatedCartItems}}}
+      )
+  }
+
   static findById(userID) {
     const db = getDb();
     return db.collection('users')
